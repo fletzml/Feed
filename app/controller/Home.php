3fe5fabc9a4ca40355a9932c4c0f3673b5979a2c
freehost <67769695+facebiik@users.noreply.github.com>
2022-11-07 14:35:21 -0600

    Update Home.php

--- app/controller/Home.php
+++ app/controller/Home.php
@@ -1,1 +1,15 @@
 <?php
+  
+    class Home extends Controller
+{ 
+    public function __construct() 
+{ 
+    $this->home = $this->model("users"); 
+} 
+    public function index() 
+{ 
+   $users = $this->home->connect(); 
+   $this->view("pages/login" , compact("users")); 
+ }
+
+}
