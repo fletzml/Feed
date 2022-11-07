9482a62f66cd6459be478a8630fd3e8c823838a4
freehost <67769695+facebiik@users.noreply.github.com>
2022-11-07 14:43:20 -0600

    Update Base.php

--- app/libs/Base.php
+++ app/libs/Base.php
@@ -7,3 +7,10 @@ protected $dbhost = DB_HOST;
 protected $dbname = DB_NAME;
 protected $dbuser = DB_USER;
 protected $dbpass = DB_PASSWORD;
+
+private $cnx;
+private $stmt;
+private $error;
+
+public function __construct() 
+{
