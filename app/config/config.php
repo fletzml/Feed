5cefaa683255d5d7f242e89252a5b271472bb241
freehost <67769695+facebiik@users.noreply.github.com>
2022-11-07 14:25:11 -0600

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,1 @@
+<?php // This is the database informationdefine("DB_HOST" , "localhost");define("DB_NAME" , "testFramework");define("DB_USER" , "root");define("DB_PASSWORD" , ""); define("URL_APP" , dirname(dirname(__FILE__)));define("URL_PROJECT" , "http://localhost/framework-mvc/");define("PROJECT_NAME" , "Framework MVC");
