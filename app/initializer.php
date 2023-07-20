5354a21e7f75752f110e653e15b2d6acf1295fa0
freehost <67769695+facebiik@users.noreply.github.com>
2023-07-20 16:34:26 -0600

    Delete app directory
    Signed-off-by: freehost <67769695+facebiik@users.noreply.github.com>

--- app/initializer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once "config/config.php";
-
-include_once "helper/helper.php";
-
-spl_autoload_register(function($file) {
-    include_once "libs/" . $file . ".php";
-});
--- app/view/pages/login.php
+++
@@ -1,2 +0,0 @@
-<h1>Welcome to the framework mvc</h1>
-<h2>Creator Kismu</h2>
